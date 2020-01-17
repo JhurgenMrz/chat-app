c1f9d6f956d71848686293ca122f29a909f95dd2
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2020-01-17 01:53:22 -0300

    Anda pero falta pulir

---
+++ client/src/components/InfoBar/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./InfoBar.css";
+import { GiCircle, GiPlainCircle } from "react-icons/gi";
+import { Link } from "react-router-dom";
+
+export const InfoBar = ({ room }) => {
+	return (
+		<div className="infoBar">
+			<div className="leftInnerContainer">
+				<GiPlainCircle style={{ color: "#42f57b", marginRight: "10px" }} />
+				<h3>{room}</h3>
+			</div>
+			<div className="rightInnerContainer">
+				<Link to="/">
+					<GiPlainCircle style={{ color: "#3b3b3b" }} />
+				</Link>
+			</div>
+		</div>
+	);
+};
---
+++ client/src/components/Messages/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
+import "./Messages.css";
+import { Message } from '../Message'
+export const Messages = ({ messages, name }) => {
+    console.log(messages);
+	return (
+		<ScrollToBottom>
+			{messages.map((message, i) => {
+				return <div key={i}>
+					<Message message={message} name={name} />
+				</div>;
+			})}
+		</ScrollToBottom>
+	);
+};
